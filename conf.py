0991f0cd8cbe53e425d1adcd8cc0ba0f13ce38f7
Steve Kieffer <sk@skieffer.info>
2024-06-10 15:48:42 -0400

    Update license headers

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 html_title = 'Proofscape Doc Modules'
-copyright = 'Copyright (c) 2018-2024 Proofscape contributors'
+copyright = 'Copyright (c) 2018-2024 Proofscape Contributors'
 html_show_sphinx = False
 
